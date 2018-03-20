bd4ab8220c577d5672fc18dfc3ae3f3decf45cb5
xdqc <qichen.d@outlook.com>
2018-03-20 21:37:37 +1300

    create match component

--- lpsolver/src/app/app.module.ts
+++ lpsolver/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MatchComponent } from './components/match/match.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatchComponent
   ],
   imports: [
     BrowserModule
